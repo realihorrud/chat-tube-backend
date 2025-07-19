2cd71c78f131bb2494c332d94b99bdeccf18790b
Ihor <Igorryd7@gmail.com>
2025-07-19 18:00:46 +0300

    feat(Telegram): add Inspiring::quote() back

--- routes/console.php
+++ routes/console.php
@@ -2,12 +2,13 @@
 
 declare(strict_types=1);
 
+use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 Artisan::command('inspire', function (): void {
-    dd(Telegram::getMe());
+    $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
 Artisan::command('set-webhook', function () {
